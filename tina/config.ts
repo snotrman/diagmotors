8ae200e0827644cf2fbe91abce87b6cf112965f9
snotrman <helikopterelidojosa@gmail.com>
2025-06-06 23:11:12 +0300

    init

--- tina/config.ts
+++ tina/config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
       publicFolder: "static",
     },
   },
-  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema
   schema: {
     collections: [
       {
